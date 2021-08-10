47ed2efc479c0772672df2947842deed456836fd
Teo Nogueira <teonogueira17@gmail.com>
2021-08-10 17:10:14 -0300

    27:00 min

--- drum.js
+++ drum.js
@@ -1,28 +1,38 @@
 document.body.addEventListener('keyup', event => {
 
-console.log(event.code)
+// console.log(event.code)
 playSound(event.code.toLowerCase())
 
 })
 
+// 3 
+document.querySelector('.composer button').addEventListener('click', (event) =>{
+let song = document.querySelector('#input').value;
+
+console.log('Música', song)
+
+
+})
+
+
 
 function playSound(sound) {
 
-let audioElement = document.querySelector(`#s_${sound}`)
+let audioElement = document.querySelector(`#s_${sound}`) // 1
 
-let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+let keyElement = document.querySelector(`div[data-key="${sound}"]`) // 2
 
-if(audioElement) {
-
+if(audioElement) { // 1 - B
+    audioElement.currentTime = 0; // preventDelay TouchKeyBoard (Important concept)
     audioElement.play();
 }
 
 
-// 2
+// 2 - B
 if(keyElement) {
 
 keyElement.classList.add('active');
-
+// 2 - C
 setTimeout(() => {
 keyElement.classList.remove('active')
 
